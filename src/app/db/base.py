e8d74922a1841cc3b20a54c212e5fe17d3df2e88
Mahmoud Seddik <mahmoud.seddik95@gmail.com>
2025-12-01 15:54:05 +0200

    Project Restructure

---
+++ src/app/db/base.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+
+#Database setup
+engine = create_engine("sqlite:///users.db", connect_args={"check_same_thread":False})
+SessionLocal = sessionmaker(autocommit=False  ,autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
