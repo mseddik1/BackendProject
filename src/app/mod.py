9923e4ff16bc71e1996ef8a9dc5de3a2407eba53
mseddik1 <160838566+mseddik1@users.noreply.github.com>
2025-12-14 06:58:11 +0200

    Inventory (#7)
    * Inventory related changes
    
    * commit

--- src/app/mod.py
+++ src/app/mod.py
@@ -11,12 +11,22 @@ with engine.connect() as conn:
     # conn.execute(text("UPDATE products SET updated_by = 'admin' WHERE updated_by = 'system'"))
     # print(conn.execute(text("SELECT * FROM users WHERE email = 'mseddik@noon.com';")).fetchall())
     # print(conn.execute(text("SELECT * FROM products WHERE id = 2;")).fetchall())
-    print(conn.execute(text("SELECT * FROM jobs ;")).fetchall())
+    # print(conn.execute(text("SELECT * FROM jobs ;")).fetchall())
+    # print(conn.execute(text("SELECT * FROM users ;")).fetchall())
+    # print(conn.execute(text("SELECT * FROM products ;")).fetchall())inventory_movements
+    print(conn.execute(text("SELECT * FROM inventory_movements ;")).fetchall())
 
-    # conn.execute(text("ALTER TABLE jobs ADD COLUMN attempts BOOLEAN DEFAULT 0;"))
-    # conn.execute(text("ALTER TABLE jobs ADD COLUMN max_attempts BOOLEAN DEFAULT 3;"))
+
+
+    # conn.execute(text("ALTER TABLE jobs ADD COLUMN attempts INTEGER DEFAULT 0;"))
+    # conn.execute(text("ALTER TABLE jobs ADD COLUMN max_attempts INTEGER DEFAULT 3;"))
     # conn.execute(text("UPDATE jobs SET status = 'pending' WHERE id = '4';"))
     # conn.execute(text("UPDATE users SET is_active = 0 WHERE email = 'mahmoud.seddik@hotmail.com';"))
+    # conn.execute(text("DELETE from users WHERE id = '9';"))
+    # conn.execute(text("ALTER TABLE products ADD COLUMN stock_quantity INTEGER DEFAULT 0;"))
+    # conn.execute(text("ALTER TABLE products DROP COLUMN in_stock ;"))
+
+
 
 
 
